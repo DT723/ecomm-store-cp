e50dc2743fc86155b8431faf58b6241cbcbb0eb5
DT723 <davidtesno@gmail.com>
2022-08-17 13:59:30 -0400

    setup firebase and creating user docs

---
+++ ecomm-store-cp/src/utils/firebase/firebase.utils.js
@@ -0,0 +1,36 @@
+import { initializeApp } from "firebase/app";
+import { getAuth, signInWithRedirect, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+
+import { getFirestore, doc, getDoc, setDoc } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAt-af5thbAB5nCe6iS9GrQEGgxweV_xks",
+  authDomain: "ecomm-store-db.firebaseapp.com",
+  projectId: "ecomm-store-db",
+  storageBucket: "ecomm-store-db.appspot.com",
+  messagingSenderId: "1053614105371",
+  appId: "1:1053614105371:web:8ada0073275ecda8f169be",
+};
+
+const firebaseApp = initializeApp(firebaseConfig);
+
+const provider = new GoogleAuthProvider();
+
+provider.setCustomParameters({
+  prompt: "select_account",
+});
+
+export const auth = getAuth();
+export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+
+export const db = getFirestore();
+
+export const createUserDocumentFromAuth = async (userAuth) => {
+  const userDocRef = doc(db, "users", userAuth.uid);
+
+  console.log(userDocRef);
+
+  const userSnapshot = await getDoc(userDocRef);
+  console.log(userSnapshot);
+  console.log(userSnapshot.exists());
+};
